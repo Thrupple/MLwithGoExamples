9840f5a8204ae050cb6feb968d0e3d1b151cca9c
Thrupple <108508285+Thrupple@users.noreply.github.com>
2020-10-11 05:36:18 +0000

    Multiple changes:

--- chapter2/chisquare_03.go
+++ chapter2/chisquare_03.go
@@ -1,4 +1,8 @@
 // Usage:
 //  go get -u gonum.org/v1/gonum/stat
 //  go get -u golang.org/x/exp/rand
-//  go run chapter2/chisquar
+//  go run chapter2/chisquare_03.go
+package main
+
+import (
+	"fla
