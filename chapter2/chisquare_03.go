60e8d20460c815ac3349f2694a0b5bf07f221785
Thrupple <108508285+Thrupple@users.noreply.github.com>
2020-11-18 07:34:39 +0000

    release 0.04a (alpha)

--- chapter2/chisquare_03.go
+++ chapter2/chisquare_03.go
@@ -20,4 +20,4 @@ func RunMain() int {
 	observed := []float64{
 		260.0, // This number is the number of observed with no regular exercise.
 		135.0, // This number is the number of observed with sporatic exercise.
-		105.0, // This number 
+		105.0, // This number is the number of observed with regular exer
