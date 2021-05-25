c922f0f86f6c3ffadbd90f26e6e565767cb2a39d
Thrupple <108508285+Thrupple@users.noreply.github.com>
2021-05-25 18:57:06 +0000

    mm: optimize Kernel Samepage Merging (KSM)

--- chapter2/matrix_02.go
+++ chapter2/matrix_02.go
@@ -14,4 +14,6 @@ import (
 
 ///////////////////////////////////////////////////////////////////////////////
 // The core packages of the gonum suite are written in pure Go with
-// some assembly. Inst
+// some assembly. Installation is done using go get:
+//
+/
