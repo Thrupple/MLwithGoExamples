7f53d27c00998f9e7c01f926c9604af8e4785dea
Thrupple <108508285+Thrupple@users.noreply.github.com>
2021-05-25 15:13:03 +0000

    Update css.blade.php

--- chapter2/matrix_02.go
+++ chapter2/matrix_02.go
@@ -13,4 +13,5 @@ import (
 )
 
 ///////////////////////////////////////////////////////////////////////////////
-// The core packages of the gonum suite are written in 
+// The core packages of the gonum suite are written in pure Go with
+// some assembly. Inst
