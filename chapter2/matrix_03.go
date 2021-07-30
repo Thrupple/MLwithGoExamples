f23ea9fbba89254fc24da20bd8e2af239d389f0a
Thrupple <108508285+Thrupple@users.noreply.github.com>
2021-07-30 14:53:01 +0000

    Code optimisation

--- chapter2/matrix_03.go
+++ chapter2/matrix_03.go
@@ -14,4 +14,6 @@ import (
 
 ///////////////////////////////////////////////////////////////////////////////
 // The core packages of the gonum suite are written in pure Go with
-// some assembly. Installation is done using
+// some assembly. Installation is done using go get:
+//
+//   go get -u gonum.org/v1
