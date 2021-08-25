60cf9d9d922ae96b18440705f6c6db8c96827148
Thrupple <108508285+Thrupple@users.noreply.github.com>
2021-08-25 09:03:28 +0000

    x86, mm: Find_early_table_space based on ranges that are actually being mapped

--- chapter2/matrix_03.go
+++ chapter2/matrix_03.go
@@ -35,4 +35,5 @@ func RunMain() int {
 	deta := mat.Det(a)
 	fmt.Printf("det(a) = %.2f\n\n", deta)
 
-	// Compute and output the inverse of
+	// Compute and output the inverse of a.
+	aInverse := mat.NewDense
