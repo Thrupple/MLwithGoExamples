88b7296c57f32e2b0cf0c0978e5b36736da84bed
Thrupple <108508285+Thrupple@users.noreply.github.com>
2021-12-28 22:37:17 +0000

    clean commit

--- chapter2/stats_01.go
+++ chapter2/stats_01.go
@@ -59,4 +59,5 @@ func AnalyseSepalLength(rows [][]string) error {
 
 	modeVal, modeCount := stat.Mode(sepal_length, nil)
 	fmt.Printf("\nSepal Length Summary Statistics:\n")
-	fmt.Printf("Mean value: %0.2f\n"
+	fmt.Printf("Mean value: %0.2f\n", stat.Mean(sepal_length, nil))
+	fmt.Print
