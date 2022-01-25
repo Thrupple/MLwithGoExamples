eddb036f9295b753736ed9fb798e5316a792a58a
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-01-25 19:56:22 +0000

    responding to review feedback

--- chapter2/stats_02.go
+++ chapter2/stats_02.go
@@ -54,3 +54,51 @@ func AnalysePetalLength(rows [][]string) error {
 		// Retrieve sepal_length
 		if petal_length_value, err := ParseFloat(row[2]); err != nil {
 			return err
+		} else {
+			petal_length = append(petal_length, petal_length_value)
+		}
+	}
+
+	// Calculate the Max of the variable.
+	minVal := floats.Min(petal_length)
+	maxVal := floats.Max(petal_length)
+
+	// Calculate the Median of the variable.
+	rangeVal := maxVal - minVal
+
+	// Calculate the variance of the variable.
+	varianceVal := stat.Variance(petal_length, nil)
+
+	// Calculate the standard deviation of the variable.
+	stdDevVal := stat.StdDev(petal_length, nil)
+
+	// Sort the values.
+	inds := make([]int, len(petal_length))
+	floats.Argsort(petal_length, inds)
+
+	// Get the Quantiles.
+	quant25 := stat.Quantile(0.25, stat.Empirical, petal_length, nil)
+	quant50 := stat.Quantile(0.50, stat.Empirical, petal_length, nil)
+	quant75 := stat.Quantile(0.75, stat.Empirical, petal_length, nil)
+
+	fmt.Printf("Petal Length Summary Statistics:\n")
+	fmt.Printf("Max value: %0.2f\n", maxVal)
+	fmt.Printf("Min value: %0.2f\n", minVal)
+	fmt.Printf("Range value: %0.2f\n", rangeVal)
+	fmt.Printf("Variance value: %0.2f\n", varianceVal)
+	fmt.Printf("Std Dev value: %0.2f\n", stdDevVal)
+	fmt.Printf("25 Quantile: %0.2f\n", quant25)
+	fmt.Printf("50 Quantile: %0.2f\n", quant50)
+	fmt.Printf("75 Quantile: %0.2f\n\n", quant75)
+
+	return nil
+}
+
+func RunMain() int {
+	if flag.NArg() != 1 {
+		log.Println("Expected file argument")
+		return -1
+	}
+	if f, err := os.Open(flag.Arg(0)); err != nil {
+		log.Println(err)
+		return -1
