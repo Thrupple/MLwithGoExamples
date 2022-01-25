0e7f88e7fd993aa597524188696b1d11b34de350
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-01-25 16:43:56 +0000

    Modularize

--- chapter2/stats_02.go
+++ chapter2/stats_02.go
@@ -15,3 +15,42 @@ import (
 	"github.com/gonum/floats"
 	"gonum.org/v1/gonum/stat"
 )
+
+///////////////////////////////////////////////////////////////////////////////
+
+var (
+	flagSkipHeader = flag.Bool("skip_header", true, "Skip CSV header row")
+)
+
+var (
+	ErrEmpty = fmt.Errorf("Empty string")
+)
+
+///////////////////////////////////////////////////////////////////////////////
+
+func ParseFloat(string_value string) (float64, error) {
+	// Check for empty value
+	if len(strings.TrimSpace(string_value)) == 0 {
+		return 0.0, ErrEmpty
+	}
+	// Convert to integer
+	if float_value, err := strconv.ParseFloat(string_value, 64); err != nil {
+		return 0.0, err
+	} else {
+		return float64(float_value), nil
+	}
+}
+
+func AnalysePetalLength(rows [][]string) error {
+	petal_length := make([]float64, 0)
+	for line_number, row := range rows {
+		if len(row) != 5 {
+			return fmt.Errorf("Line %v: Expected 5 values", line_number+1)
+		}
+		// Skip header
+		if *flagSkipHeader && line_number == 0 {
+			continue
+		}
+		// Retrieve sepal_length
+		if petal_length_value, err := ParseFloat(row[2]); err != nil {
+			return err
