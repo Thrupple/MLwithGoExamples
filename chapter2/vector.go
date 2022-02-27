efc9e92606c70797a802b5acf046474c623c359e
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-02-27 20:40:59 +0000

    add headers

--- chapter2/vector.go
+++ chapter2/vector.go
@@ -13,4 +13,5 @@ import (
 )
 
 ///////////////////////////////////////////////////////////////////////////////
-// The core packages of the gonum suite are written
+// The core packages of the gonum suite are written in pure Go with
+// some assembly. Installatio
