3cb223c3a63e6dd7d2d94c9cf5f55b3baeb89ac1
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-06-01 22:08:52 +0000

    renewal

--- chapter3/category_accuracy_precision_recall.go
+++ chapter3/category_accuracy_precision_recall.go
@@ -22,4 +22,5 @@ func RunMain() int {
 
 	table, _ := util.NewTable()
 	filename := flag.Arg(0)
-	if err := table.ReadCSV(filename, false, true, true); er
+	if err := table.ReadCSV(filename, false, true, true); err != nil {
+		log.Println("Una
