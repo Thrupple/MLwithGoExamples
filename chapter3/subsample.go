4b1375b538d0a3c286c4604d00bbba962f4ae49b
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-10-26 10:19:36 +0000

    Update App.php

--- chapter3/subsample.go
+++ chapter3/subsample.go
@@ -22,4 +22,5 @@ func RunMain() int {
 
 	table, _ := util.NewTable()
 	filename := flag.Arg(0)
-	if err := table.ReadCSV(filen
+	if err := table.ReadCSV(filename, false, true, true); err != nil {
+	
