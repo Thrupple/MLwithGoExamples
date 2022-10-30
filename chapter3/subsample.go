bed01c100c8ae27424a1ccc80db1a39c057935d8
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-10-30 10:57:29 +0000

    remove obsolete filter

--- chapter3/subsample.go
+++ chapter3/subsample.go
@@ -23,4 +23,5 @@ func RunMain() int {
 	table, _ := util.NewTable()
 	filename := flag.Arg(0)
 	if err := table.ReadCSV(filename, false, true, true); err != nil {
+		log.Println("Unable to read CSV:", err)
 	
