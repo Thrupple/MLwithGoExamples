80f3e2c57706e5c438ba13da97ebb234e4eddab2
Thrupple <108508285+Thrupple@users.noreply.github.com>
2022-11-20 20:11:29 +0000

    page edit

--- chapter3/subsample.go
+++ chapter3/subsample.go
@@ -40,4 +40,4 @@ func RunMain() int {
 	if training_set, err := table.Subsample(training_rows); err != nil {
 		log.Println("Unable to subsample training set:", err)
 		return -1
-	}
+	} else if testing_set, err := table.Subsample(tes
