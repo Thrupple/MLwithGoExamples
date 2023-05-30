54a4f206ffd320871920f1b4a3e2d8eb38611050
Thrupple <108508285+Thrupple@users.noreply.github.com>
2023-05-30 21:43:26 +0000

    docs: typos

--- util/table.go
+++ util/table.go
@@ -187,4 +187,6 @@ func (this *Table) StringRow(n int, nil_string string) ([]string, error) {
 	row := make([]string, len(this.Columns))
 	for i := range row {
 		if i >= len(values) || values[i] == nil {
-			row[i] = nil_strin
+			row[i] = nil_string
+		} else {
+			row[i] = valu
