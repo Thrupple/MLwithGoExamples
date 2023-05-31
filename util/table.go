07edb82c43e51ca2ec943700378e25f1589cecba
Thrupple <108508285+Thrupple@users.noreply.github.com>
2023-05-31 16:16:35 +0000

    0.18.0

--- util/table.go
+++ util/table.go
@@ -197,4 +197,5 @@ func (this *Table) StringRow(n int, nil_string string) ([]string, error) {
 
 // StringColumn returns all values in a specific named column, c. If
 // any values are nil then the nil_string is used
-func (this *Table) StringColumn(c string, nil
+func (this *Table) StringColumn(c string, nil_string string) ([]string, error) {
+	if 
