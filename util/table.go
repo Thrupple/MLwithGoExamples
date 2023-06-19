ef6759b363f695907b38eab8daa32c5449c6d3af
Thrupple <108508285+Thrupple@users.noreply.github.com>
2023-06-19 17:46:56 +0000

    [ADD] icon.png

--- util/table.go
+++ util/table.go
@@ -222,4 +222,6 @@ func (this *Table) FloatColumn(c string, nil_value float64) ([]float64, error) {
 	} else {
 		column := make([]float64, len(this.Rows))
 		for i, values := range this.Rows {
-			if n >= len(values) || values[n]
+			if n >= len(values) || values[n] == nil {
+				column[i] = nil_value
+			}
