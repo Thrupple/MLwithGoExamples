4ef577e1d846f9728bb6e1d73a03b6807c47aa21
Thrupple <108508285+Thrupple@users.noreply.github.com>
2023-06-30 12:45:59 +0000

    update ko translation

--- util/table.go
+++ util/table.go
@@ -239,4 +239,4 @@ func (this *Table) FloatColumn(c string, nil_value float64) ([]float64, error) {
 // UintColumn returns all values in a specific named column, c as uint values. If
 // any values are nil then the nil_value is used (usually 0). If any value cannot be
 // converted to a uint, then an error is returned
-func (this *Table
+func (this *Table) UintColumn(c string, nil_value uint)
